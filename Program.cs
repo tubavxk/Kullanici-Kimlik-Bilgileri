aa67ba0199836b7a228cc6263e0d3f6b3bb0e171
Tuba <tubaazsoy@gmail.com>
2024-11-21 00:08:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Adınızı girin:");
+string ad = Console.ReadLine();
+
+Console.WriteLine("Soyadınızı girin:");
+string soyad = Console.ReadLine();
+
+Console.WriteLine("Öğrenci numaranızı girin:");
+string ogrenciNo = Console.ReadLine();
+
+Console.WriteLine("Cep telefonunuzu girin:");
+string cepTel = Console.ReadLine();
+
+Console.WriteLine("Girdiğiniz Bilgiler");
+Console.WriteLine("Ad: " + ad);
+Console.WriteLine("Soyad: " + soyad);
+Console.WriteLine("Öğrenci No: " + ogrenciNo);
+Console.WriteLine("Cep Tel: " + cepTel);
